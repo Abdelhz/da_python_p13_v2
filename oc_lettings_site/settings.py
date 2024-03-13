72b1a5b468cb58935b7d507192cfe2ef7eabcde9
Shinkhan <hzabdelwahid@gmail.com>
2024-03-13 10:49:32 +0100

    Correcting issue in Allowed HOSTS in settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -18,7 +18,7 @@ SENTRY_DSN = os.environ.get('SENTRY_DSN')
 DEBUG = False
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['host', '127.0.0.1', '*.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['host', '127.0.0.1', 'localhost', 'da-python-p13-oc-lettings-site-prod.onrender.com']
 
 
 # Application definition
