5ddbfd637a85c5714f78a9f2f47d1955c94c9040
Shinkhan <hzabdelwahid@gmail.com>
2024-03-13 10:08:04 +0100

    Corrected linting error in settings.

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -17,7 +17,7 @@ SENTRY_DSN = os.environ.get('SENTRY_DSN')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-#ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['host', '127.0.0.1', '*.onrender.com', 'localhost']
 
 
